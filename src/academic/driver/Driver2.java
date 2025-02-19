83be64e4049165fff19fc5a560f71fe7df47ff96
AndikaNadapdap <andikanadapdap02@gmail.com>
2025-02-19 17:54:35 +0700

    21

--- src/academic/driver/Driver2.java
+++ src/academic/driver/Driver2.java
@@ -1,16 +1,17 @@
 package academic.driver;
 
 import academic.model.Course;
-import academic.model.Enrollment;
 import academic.model.Student;
+import academic.model.Enrollment;
 import java.util.Scanner;
 
 /**
-
+ * @author 12S23009_Dina Marlina Siagian
+ * @author 12S23028_Daniel Situmorang
  */
 public class Driver2 {
     public static void main(String[] _args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         Course[] courses = new Course[100];
         Student[] students = new Student[100];
@@ -23,102 +24,93 @@ public class Driver2 {
         StringBuilder invalidEntries = new StringBuilder();
 
         while (true) {
-            String line = scanner.nextLine().trim();
+            String line = input.nextLine().trim();
 
             if (line.equals("---")) {
                 break;
             }
 
-            String[] parts = line.split("#");
-            if (parts.length > 0) {
-                String command = parts[0];
+            String[] data = line.split("#");
 
-                switch (command) {
-                    case "course-add":
-                        if (parts.length == 5) {
-                            String code = parts[1];
-                            String name = parts[2];
-                            String credits = parts[3];
-                            String grade = parts[4];
-                            courses[courseCount++] = new Course(code, name, credits, grade);
-                        } else {
-                            invalidEntries.append("invalid course-add command|").append(line).append("\n");
-                        }
-                        break;
-
-                    case "student-add":
-                        if (parts.length == 5) {
-                            String code = parts[1];
-                            String name = parts[2];
-                            String year = parts[3];
-                            String major = parts[4];
+            switch (data[0]) {
+                case "course-add":
+                    if (data.length == 5) {
+                        String code = data[1];
+                        String name = data[2];
+                        String credits = data[3];
+                        String grade = data[4];
+                        courses[courseCount++] =  new Course(code, name, credits, grade);     
+                    }
+                    break;
+                case "student-add":
+                    if (data.length == 5) {
+                        String code = data[1];
+                            String name = data[2];
+                            String year = data[3];
+                            String major = data[4];
                             students[studentCount++] = new Student(code, name, year, major);
-                        } else {
-                            invalidEntries.append("invalid student-add command|").append(line).append("\n");
+                    }
+                    break;
+                case "enrollment-add":
+                    if (data.length == 5) {
+                        String coursecode = data[1];
+                        String studentnim = data[2];
+                        
+                        boolean courseExists = false;
+                        boolean studentExists = false;
+                        
+                        for (int i = 0; i < courseCount; i++) {
+                            if (courses[i].getCode().equals(coursecode)) {
+                                courseExists = true;
+                                break;
+                            }
                         }
-
-                        break;
-
-                    case "enrollment-add":
-                        if (parts.length == 5) {
-                            String courseCode = parts[1];
-                            String studentId = parts[2];
-                            String year = parts[3];
-                            String semester = parts[4];
+                        
+                        for (int i = 0; i < studentCount; i++) {
+                            if (students[i].getCode().equals(studentnim)) {
+                                studentExists = true;
+                                break;
+                            }
+                        }
+                        
+                        if (!courseExists) {
+                            invalidEntries.append("invalid course|").append(coursecode).append("\n");
+                        } else if (!studentExists) {
+                            invalidEntries.append("invalid student|").append(studentnim).append("\n");
+                        } else {
+                            String courseCode = data[1];
+                            String studentId = data[2];
+                            String year = data[3];
+                            String semester = data[4];
                             String Notes = "None";
-
-                            boolean courseExists = false;
-                            boolean studentExists = false;
-
-                            for (int i = 0; i < courseCount; i++) {
-                                if (courses[i].getCode().equals(courseCode)) {
-                                    courseExists = true;
-                                    break;
-                                }
-                            }
-
-                            for (int i = 0; i < studentCount; i++) {
-                                if (students[i].getCode().equals(studentId)) {
-                                    studentExists = true;
-                                    break;
-                                }
-                            }
-
-                            if (!courseExists) {
-                                invalidEntries.append("invalid course|").append(courseCode).append("\n");
-                            } else if (!studentExists) {
-                                invalidEntries.append("invalid student|").append(studentId).append("\n");
-                            } else {
-                                enrollments[enrollmentCount++] = new Enrollment(courseCode, studentId, year, semester, Notes);
-                            }
-                        } else {
-                            invalidEntries.append("invalid enrollment-add command|").append(line).append("\n");
+                            enrollments[enrollmentCount++] = new Enrollment(courseCode, studentId, year, semester, Notes);
+                           
                         }
-                        break;
-
-                    default:
-                        invalidEntries.append("unknown command|").append(command).append("\n");
-                }
+                    }
+                    break;
+                default:
+                    System.out.println("Error: Perintah tidak dikenali.");
             }
         }
 
-        scanner.close();
+        input.close();
 
         System.out.print(invalidEntries.toString());
 
-  
+      
         for (int i = courseCount - 1; i >= 0; i--) {
             System.out.println(courses[i].toString());
         }
 
 
-        for (int i = studentCount - 1; i >= 0; i--) {
-            System.out.println(students[i]);
+        for (int i = 0 ; i < studentCount ; i++) {
+            System.out.println(students[i].toString());
         }
+
 
         for (int i = 0; i < enrollmentCount; i++) {
             System.out.println(enrollments[i].toString());
-
         }
+        
     }
 }
